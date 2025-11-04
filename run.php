4ee51feb378a45db2512b6ab8ea89c122d9d8880
new-CarlosEst <c.estebandiez@gmail.com>
2025-11-04 16:18:34 +0100

    comentado que hace run.php y todas sus lineas
    Comentado que hace el archivo run.php
    
    Comentado que hace y que recibe la funcion error_reporting, ini_set y init
    
    Ademas comento que imports hace el archivo

--- run.php
+++ run.php
@@ -1,11 +1,27 @@
 <?php
+//Run.php lo que es como un main que ejecutara todo
 
-
+/**
+ * Error reporting es una funcion para activar errores y le paso los errores a activar
+ * 
+ * @var int $level Constante que dice los errores, en este caso E_ALL tiene todos los errores de php
+ */
 error_reporting(E_ALL);
+/**
+ * Con init set cambio la configuracion de php
+ * 
+ * @var string Le paso que muestre todos los errores
+ * @var int EL numero es si los muestra o no, 1- mostrar | 0-ocultar
+ */
 ini_set('display_errors', 1);
 
+/**
+ * Hago los require necesarios para que vayan todas las funciones tanto de config.php como de functions.php
+ */
 require __DIR__ . '/includes/config.php';
 require __DIR__ . '/includes/functions.php';
 
-
+/**
+ * Llamo a la funcion init de functions.php
+ */
 init();
